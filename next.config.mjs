636ebbf308c1614d2c018e00da18d553632b8c0b
anclaev <iahugo@yandex.ru>
2024-08-11 03:05:34 +0300

    feat(app): Страница 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin(
-  './app/common/locales/i18n.ts'
-);
+  './app/common/locales/i18n.ts',
+)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   optimizeFonts: true,
   output: 'standalone',
   compress: true,
-  reactStrictMode: true,
+  reactStrictMode: true
 }
 
 export default withNextIntl(nextConfig)
